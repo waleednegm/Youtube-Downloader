1edb71083ed092d1b9ae1476f51627d780a5401a
Waleed Negm <55960681+waleednegm@users.noreply.github.com>
2021-01-21 13:31:48 +0200

    Update video.py

--- video.py
+++ video.py
@@ -2,21 +2,12 @@ import downloader
 
 def download():
     youtube_link = input("Enter The Link of youtube video ? ")
-    print("1 - 144p")
-    print("2 - 240p")
-    print("3 - 360p")
-    print("4 - 480p")
-    print("5 - 720p")
-    quality = input("choose [1-5]:  ")
-    if(quality =="1"):
-        downloader.download_video(youtube_link,"144p")
-    elif (quality =="2"):
-        downloader.download_video(youtube_link,"240p")
-    elif (quality =="3"):
+    print("1 - 360p")
+    print("2 - 720p")
+    quality = input("choose [1-2]:  ")
+    if (quality =="1"):
         downloader.download_video(youtube_link,"360p")
-    elif (quality =="4"):
-        downloader.download_video(youtube_link,"480p")
-    elif (quality =="5"):
+    elif (quality =="2"):
         downloader.download_video(youtube_link,"720p")
     else:
         print("Wrong Choice")
