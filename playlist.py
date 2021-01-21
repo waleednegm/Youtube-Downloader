eb88d63fe904a352b7dad794557e4c374a8eca8c
Waleed Negm <55960681+waleednegm@users.noreply.github.com>
2021-01-21 13:32:44 +0200

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -3,24 +3,15 @@ from pytube import Playlist
 
 def download():
     youtube_link = input("Enter The Link of youtube playlist ? ")
-    print("1 - 144p")
-    print("2 - 240p")
-    print("3 - 360p")
-    print("4 - 480p")
-    print("5 - 720p")
+    print("1 - 360p")
+    print("2 - 720p")
     quality = input("choose [1-5]:  ")
     from pytube import Playlist
     pl = Playlist(youtube_link)
     for video in pl.videos:
-        if(quality =="1"):
-            playlist_downloader.download_video(video,"144p")
-        elif (quality =="2"):
-            playlist_downloader.download_video(video,"240p")
-        elif (quality =="3"):
+        if (quality =="1"):
             playlist_downloader.download_video(video,"360p")
-        elif (quality =="4"):
-            playlist_downloader.download_video(video,"480p")
-        elif (quality =="5"):
+        elif (quality =="2"):
             playlist_downloader.download_video(video,"720p")
         else:
             print("Error")
