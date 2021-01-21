788c2892e759229c49ee36841ba1612cd4676b68
Waleed Negm <55960681+waleednegm@users.noreply.github.com>
2021-01-21 13:33:00 +0200

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -2,7 +2,7 @@ import pytube
 
 def download_video(youtube_link,quailty):
     try:
-        pytube.YouTube(youtube_link).streams.filter(res=quailty).first().download()
+        pytube.YouTube(youtube_link).streams.filter(res=quailty,progressive=True).first().download()
         print("The video has been downloaded")
     except :
         print("This quailty not available for this video")
